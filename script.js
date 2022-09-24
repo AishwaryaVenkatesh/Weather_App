6bf4773717403c2f30b916bd48218bbffb520d2e
Aishwarya <33971260+AishwaryaVenkatesh@users.noreply.github.com>
2022-09-24 09:06:55 -0700

    Fixing issues with wind

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ function displayWeather(response) {
 
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#wind").innerHTML = Math.round(
-    response.data.main.wind
+    response.data.wind.speed
   );
 }
 
